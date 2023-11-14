10a054c8409cea4e7b119c883d704efda2b4dab8
german <gerchureloco@gmail.com>
2023-11-14 06:32:46 -0800

    main.c

--- _printf.c
+++ _printf.c
@@ -10,11 +10,25 @@
 int _printf(const char *format, ...)
 {
 va_list parameters;
-int i;
-va_start(parameters, format);
-for ( i = 0, i < strlen(format) ; i++)
-{
-	
-}
+int i, length;
+char c;
 
+va_start(parameters, format);
+for (i = 0 ; i < strlen(format) ; i++)
+{	
+	if (format[i] != NULL)
+	{
+		if (format[i] == % && (format[i + 1] == c || format[i + 1] == i	|| format[i + 1] == s || format[i + 1] == d))
+			{	
+			length += get_op_func(parameters, format[i + 1]);
+			i++;
+			}
+		else
+			{
+			_putchar(format[i]);
+			length++;
+			}
+	}
+}
+return (length);
 }
